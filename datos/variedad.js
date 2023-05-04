08987fb87906cb53ec0be64e3252d02b684b6ed3
Darilee2604 <88863409+LiscyViera@users.noreply.github.com>
2023-05-04 16:52:59 -0600

    si funciona no le mueva

--- datos/variedad.js
+++ datos/variedad.js
@@ -2,96 +2,22 @@ let infoVariedad = {
   'banda': [
     {
       Variedad: [
-        {
-          nombre: 'NSA',
-          hojas_gavilla: 38,
-          gavillas_fundas: 37,
-          hojas_fundas: 1406
-        },
-        {
-          nombre: 'corojo grande',
-          hojas_gavilla: 38,
-          gavillas_fundas: 37,
-          hojas_fundas: 1406
-        },
-        {
-          nombre: 'corojo mediano',
-          hojas_gavilla: 38,
-          gavillas_fundas: 37,
-          hojas_fundas: 1406
-        },
-        {
-          nombre: 'Corojo sol 15',
-          hojas_gavilla: 40,
-          gavillas_fundas: 37,
-          hojas_fundas: 1480
-        },
-        {
-          nombre: 'Criollo 98 LM',
-          hojas_gavilla: 39,
-          gavillas_fundas: 38,
-          hojas_fundas: 1482
-        },
-        {
-          nombre: 'habano criollo grande',
-          hojas_gavilla: 39,
-          gavillas_fundas: 38,
-          hojas_fundas: 1482
-        },
-        {
-          nombre: 'habano criollo mediano',
-          hojas_gavilla: 39,
-          gavillas_fundas: 38,
-          hojas_fundas: 1482
-        },
-        {
-          nombre: 'Habano jalapa grande',
-          hojas_gavilla: 39,
-          gavillas_fundas: 38,
-          hojas_fundas: 1482
-        },
-        {
-          nombre: 'Habano jalapa mediano',
-          hojas_gavilla: 39,
-          gavillas_fundas: 38,
-          hojas_fundas: 1482
-        },
-        {
-          nombre: 'Sumatra tripa',
-          hojas_gavilla: 40,
-          gavillas_fundas: 37,
-          hojas_fundas: 1480
-        },
-        {
-          nombre: 'Corojo 2016',
-          hojas_gavilla: 38,
-          gavillas_fundas: 37,
-          hojas_fundas: 1406
-        },
-        {
-          nombre: 'Criollo sol (antes capa)',
-          hojas_gavilla: 38,
-          gavillas_fundas: 37,
-          hojas_fundas: 1406
-        },
-        {
-          nombre: 'Generoso (antes capa)',
-          hojas_gavilla: 38,
-          gavillas_fundas: 37,
-          hojas_fundas: 1406
-        },
-        {
-          nombre: 'H-62',
-          hojas_gavilla: 38,
-          gavillas_fundas: 37,
-          hojas_fundas: 1406
-        },
-        {
-          nombre: 'Pennsylvania (tripa)',
-          hojas_gavilla: 22,
-          gavillas_fundas: 59,
-          hojas_fundas: 1298
-        }
+        { nombre: 'NSA', hojas_fundas: 1406 },
+  { nombre: 'Corojo grande', hojas_fundas: 1406 },
+  { nombre: 'Corojo mediano', hojas_fundas: 1406 },
+  { nombre: 'Corojo sol 15', hojas_fundas: 1480 },
+  { nombre: 'Criollo 98 LM', hojas_fundas: 1482 },
+  { nombre: 'Habano criollo grande', hojas_fundas: 1482 },
+  { nombre: 'Habano criollo mediano', hojas_fundas: 1482 },
+  { nombre: 'Habano jalapa grande', hojas_fundas: 1482 },
+  { nombre: 'Habano jalapa mediano', hojas_fundas: 1482 },
+  { nombre: 'Sumatra tripa', hojas_fundas: 1480 },
+  { nombre: 'Corojo 2016', hojas_fundas: 1406 },
+  { nombre: 'Criollo sol (antes capa)', hojas_fundas: 1406 },
+  { nombre: 'Generoso (antes capa)', hojas_fundas: 1406 },
+  { nombre: 'H-62', hojas_fundas: 1406 },
+  { nombre: 'Pennsylvania (tripa)', hojas_fundas: 1298 }
+
       ]
     }
   ],
